ffd3f8ec4c9e43b8107d85498b5c4f0c7d1f7f3b
Artyom_PC <lilapank@yandex.ru>
2024-01-26 23:02:54 +0300

    add: script for send order

--- cmd/order_sender/order_sender.go
+++ cmd/order_sender/order_sender.go
@@ -1,4 +1,38 @@
-package ordersender
+package main
 
+import (
+	"fmt"
+	"log"
+	"time"
 
-func main(){}
+	stan "github.com/nats-io/stan.go"
+)
+
+const (
+	clusterID = "wb_orders"
+	clientID  = "order_sender"
+	channel   = "order"
+)
+
+func main() {
+	sc, err := stan.Connect(
+		clusterID,
+		clientID,
+		stan.NatsURL(stan.DefaultNatsURL))
+	if err != nil {
+		log.Printf("[ERROR]: %s", err.Error())
+		return
+	}
+	defer sc.Close()
+
+	for i := 0; ; i++ {
+		msg := fmt.Sprintf("Hello %d", i)
+		err := sc.Publish(channel, []byte(msg))
+		if err != nil {
+			log.Printf("[ERROR]: %s", err.Error())
+			return
+		}
+		fmt.Println("I send:" + msg)
+		time.Sleep(5 * time.Second)
+	}
+}
