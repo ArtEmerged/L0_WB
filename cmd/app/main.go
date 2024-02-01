712be2a5854290d78d1c964c01ba573eb055c55b
Artyom_PC <lilapank@yandex.ru>
2024-02-01 18:28:40 +0300

    add: func for start app

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,42 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"time"
-
-	"github.com/nats-io/stan.go"
-)
-
-const (
-	clusterID = "wb_orders"
-	clientID  = "order_recipient"
-	channel   = "order"
-	durableID = "wb-durable"
+	"flag"
+	"wblzero/internal/app"
 )
 
 func main() {
-	sc, err := stan.Connect(
-		clusterID,
-		clientID)
-	if err != nil {
-		log.Printf("[ERROR]: %s", err.Error())
-		return
-	}
-	defer sc.Close()
-	for i := 0; i < 10; i++ {
-		sub, err := sc.Subscribe(channel, func(msg *stan.Msg) {
-			data := string(msg.Data)
-			fmt.Println(data)
-			msg.Ack()
-		},
-			stan.DurableName(durableID),
-			stan.DeliverAllAvailable())
-		if err != nil {
-			log.Printf("[ERROR]: %s", err.Error())
-			return
-		}
-		time.Sleep(5 * time.Second)
-		sub.Close()
-	}
+	cfgPath := flag.String("cfg", "./wb.env", "USAGE -cfg='path_to_config_file'")
+	flag.Parse()
+	app.RunServer(*cfgPath)
 }
