947489f02e50d44d6cf562f12bfe60e4aee15a09
Artyom_PC <lilapank@yandex.ru>
2024-02-01 18:40:19 +0300

    add: func for subscribe and shutdown nats

---
+++ internal/server/nats.go
@@ -0,0 +1,44 @@
+package server
+
+import (
+	"wblzero/config"
+	"wblzero/internal/nats"
+
+	"github.com/nats-io/stan.go"
+	"github.com/sirupsen/logrus"
+)
+
+type Subscriber struct {
+	conn stan.Conn
+	sub  stan.Subscription
+}
+
+func NewSubscribe(cfg config.Nats, handler *nats.Handler) *Subscriber {
+	const clientId = "order_recipient"
+
+	sc, err := stan.Connect(
+		cfg.ClusterId,
+		clientId)
+	if err != nil {
+		logrus.Error(err.Error())
+	}
+	logrus.Infof("connected to Nats Streaming %s clusterID: [%s] clientID: [%s]", stan.DefaultNatsURL, cfg.ClusterId, clientId)
+
+	sub, err := sc.Subscribe(
+		cfg.Channel,
+		handler.SaveOrder,
+		stan.DurableName(cfg.DurableId),
+		stan.DeliverAllAvailable(),
+	)
+	if err != nil {
+		sc.Close()
+		logrus.Error(err.Error())
+	}
+	return &Subscriber{conn: sc, sub: sub}
+}
+
+func (s *Subscriber) ShutdownNats() error {
+	err := s.sub.Close()
+	err = s.conn.Close()
+	return err
+}
