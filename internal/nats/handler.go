23c6f7f95f2cfa16b4e878d5917ec28b341603b8
Artyom_PC <lilapank@yandex.ru>
2024-02-01 18:58:57 +0300

    add: nats handler for save order

---
+++ internal/nats/handler.go
@@ -0,0 +1,33 @@
+package nats
+
+import (
+	"encoding/json"
+	"wblzero/internal/models"
+	"wblzero/internal/service"
+
+	"github.com/nats-io/stan.go"
+	"github.com/sirupsen/logrus"
+)
+
+type Handler struct {
+	service *service.Service
+}
+
+func NewHandler(service *service.Service) *Handler {
+	return &Handler{service: service}
+}
+
+func (h *Handler) SaveOrder(msg *stan.Msg) {
+	order := new(models.Order)
+	err := json.Unmarshal(msg.Data, order)
+	if err != nil {
+		logrus.Error(err.Error())
+		return
+	}
+	err = h.service.Add(order)
+	if err != nil {
+		logrus.Errorf("order %s could not be added to the database\n%s", order.OrderUID, err.Error())
+		return
+	}
+	logrus.Infof("order:%s successfully added to database", order.OrderUID)
+}
